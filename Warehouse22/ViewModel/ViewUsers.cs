3813cf82c62f4d7d0ba184982a13ae5ad7cdb37b
EladAM52 <93151656+EladAM52@users.noreply.github.com>
2023-08-02 21:29:17 +0300

    Add files via upload

---
+++ Warehouse22/ViewModel/ViewUsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WareHouse22.Models;
+
+namespace WareHouse22.ViewModel
+{
+    public class UsersViewModel
+    {
+
+        public tblUsers ShowUser { get; set; }
+
+        public List<tblUsers> ShowUsers { get; set; }
+
+    }
+
+}
